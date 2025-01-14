9f33fa563da170db162e35d4ef674bfde228d2a4
Umut <umut.gu@outlook.com>
2025-01-15 02:10:26 +0300

    service impl

--- src/main/java/com/turkcell/pair4project/service/KeepingUserActivityLogsServiceImpl.java
+++ src/main/java/com/turkcell/pair4project/service/KeepingUserActivityLogsServiceImpl.java
@@ -1,24 +1,31 @@
 package com.turkcell.pair4project.service;
 
 import com.turkcell.pair4project.entity.KeepingUserActivityLog;
+import com.turkcell.pair4project.repository.KeepingUserActivityLogsRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class KeepingUserActivityLogsServiceImpl implements KeepingUserActivityLogsService {
+    private final KeepingUserActivityLogsRepository keepingUserActivityLogsRepository;
+
+    public KeepingUserActivityLogsServiceImpl(KeepingUserActivityLogsRepository keepingUserActivityLogsRepository) {
+        this.keepingUserActivityLogsRepository = keepingUserActivityLogsRepository;
+    }
     @Override
     public KeepingUserActivityLog add(KeepingUserActivityLog keepingUserActivityLog) {
-        return null;
+        keepingUserActivityLogsRepository.save(keepingUserActivityLog);
+        return keepingUserActivityLog;
     }
 
     @Override
     public List<KeepingUserActivityLog> getAll() {
-        return List.of();
+        return keepingUserActivityLogsRepository.findAll();
     }
 
     @Override
     public KeepingUserActivityLog getById(int id) {
-        return null;
+        return keepingUserActivityLogsRepository.findById(id).orElse(null);
     }
 }
